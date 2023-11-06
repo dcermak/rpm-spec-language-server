eabf7289c944aa8e58d386cc0fdbb61389c4d0c9
Dan Čermák <dcermak@suse.com>
2023-11-06 16:30:46 +0100

    refactor DocumentSymbol creation to allow for testing

--- rpm_spec_language_server/server.py
+++ rpm_spec_language_server/server.py
@@ -1,6 +1,5 @@
 from urllib.parse import urlparse
 from specfile.macros import Macros
-from specfile.specfile import Specfile
 from lsprotocol.types import (
     TEXT_DOCUMENT_COMPLETION,
     TEXT_DOCUMENT_DOCUMENT_SYMBOL,
@@ -10,12 +9,11 @@ from lsprotocol.types import (
     CompletionParams,
     DocumentSymbol,
     DocumentSymbolParams,
-    Position,
-    Range,
     SymbolInformation,
-    SymbolKind,
 )
 from pygls.server import LanguageServer
+
+from rpm_spec_language_server.document_symbols import spec_to_document_symbols
 
 
 class RpmSpecLanguageServer(LanguageServer):
@@ -45,50 +43,6 @@ def create_rpm_lang_server() -> RpmSpecLanguageServer:
         if url.scheme != "file" or not url.path.endswith(".spec"):
             return None
 
-        symbols = []
-
-        spec = Specfile(url.path)
-        with spec.sections() as sections:
-            current_line = 0
-
-            for section in sections:
-                name = section.name
-
-                if opt := str(section.options).strip():
-                    if not opt.startswith("-"):
-                        name = f"{name} {spec.name}-{opt.split()[0]}"
-
-                    if "-n" in opt:
-                        name = f"{name} {(o := opt.split())[o.index('-n') + 1]}"
-
-                section_length = len(section.data)
-
-                if name != "package":
-                    section_length += 1
-
-                symbols.append(
-                    DocumentSymbol(
-                        name=name,
-                        kind=SymbolKind.Namespace,
-                        range=(
-                            section_range := Range(
-                                start=Position(line=current_line, character=0),
-                                end=Position(
-                                    line=current_line + section_length, character=0
-                                ),
-                            )
-                        ),
-                        selection_range=section_range
-                        if name == "package"
-                        else Range(
-                            start=Position(line=current_line, character=0),
-                            end=Position(line=current_line + 1, character=0),
-                        ),
-                    )
-                )
-
-                current_line += section_length
-
-        return symbols
+        return spec_to_document_symbols(url.path)
 
     return rpm_spec_server
